a64ed91e90779bf43dae1a0a22f0deb4550f819a
Meriem-bounaas <bouunaas.meriem.1993@gmail.com>
2022-11-03 18:33:49 +0100

    made it responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    colors:{
-      'first-color': '#7BB872',
-      'second-color': '#C4C4C4',
-      'third-color': '#818181',
-      'forth-color' : '#FFFFFF',
+    extend: {
+      colors:{
+        'first-color': '#7BB872',
+        'second-color': '#C4C4C4',
+        'third-color': '#818181',
+        'forth-color' : '#FFFFFF',
+        'fifth-color': '#eee'
+      },
+      fontFamily:{
+        'my-font': ['Montserrat'],
+        'font-text': ['Raleway'],
+      },
     },
-    fontFamily:{
-      'my-font': ['Montserrat'],
-    },
-    extend: {},
   },
   plugins: [],
 }
